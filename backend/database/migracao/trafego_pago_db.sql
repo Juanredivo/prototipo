8d4b58c1664044445b3c76f567b740d6ad7039ab
Juan Redivo <juanredivo@hotmail.com>
2023-06-20 00:31:08 -0300

    feat/ atualizando a database

--- backend/database/migracao/trafego_pago_db.sql
+++ backend/database/migracao/trafego_pago_db.sql
@@ -1,13 +1,55 @@
 CREATE DATABASE nome_do_banco_de_dados;
 USE nome_do_banco_de_dados;
 
-CREATE TABLE Users (
-  id_user INT PRIMARY KEY AUTO_INCREMENT,
-  nm_full_name VARCHAR(255),
-  nm_email VARCHAR(255),
-  nu_phone_number VARCHAR(20),
-  nm_address VARCHAR(255)
+CREATE TABLE `users` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `email` (`email`)
 );
+
+CREATE TABLE `ramos` (
+  `id_ramo` int NOT NULL AUTO_INCREMENT,
+  `nome_ramo` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_ramo`),
+  UNIQUE KEY `nome_ramo` (`nome_ramo`)
+);
+
+CREATE TABLE `planospersonalizados` (
+  `id_plano_personalizado` int NOT NULL AUTO_INCREMENT,
+  `id_plano` int DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `facebook_ads` tinyint(1) DEFAULT NULL,
+  `insta_ads` tinyint(1) DEFAULT NULL,
+  `youtube_ads` tinyint(1) DEFAULT NULL,
+  `google_ads` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id_plano_personalizado`),
+  KEY `id_plano` (`id_plano`),
+  CONSTRAINT `planospersonalizados_ibfk_1` FOREIGN KEY (`id_plano`) REFERENCES `planos` (`id_plano`)
+);
+
+CREATE TABLE `planos` (
+  `id_plano` int NOT NULL AUTO_INCREMENT,
+  `id_ramo` int DEFAULT NULL,
+  `nome_plano` varchar(100) DEFAULT NULL,
+  `descricao` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id_plano`),
+  KEY `id_ramo` (`id_ramo`),
+  CONSTRAINT `planos_ibfk_1` FOREIGN KEY (`id_ramo`) REFERENCES `ramos` (`id_ramo`)
+);
+
+CREATE TABLE `mensagens` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mensagem` text NOT NULL,
+  `data_envio` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
 
 CREATE TABLE Payments (
   id_payment INT PRIMARY KEY AUTO_INCREMENT,
